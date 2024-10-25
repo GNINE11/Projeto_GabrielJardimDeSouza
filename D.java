43c58b6a153198ef27f23526e02f681caf4dfbdc
Gabriel Jardim <gabriel.souza16@estudante.ufla.br>
2024-10-25 20:15:30 -0300

    Criacao da classe D

---
+++ D.java
@@ -0,0 +1,28 @@
+public class D{
+    private String D1;
+    private double D2;
+
+    public String getD1(){
+        return D1;
+    }
+
+    public double getD2(){
+        return D2;
+    }
+
+    public void setD1(String newValueD1){
+        D1 = newValueD1;
+    }
+
+    public void setD2(double newValueD2){
+        D2 = newValueD2;
+    }
+
+    public void MD1(){
+        System.out.println("MD1");
+    }
+
+    public void MD2(){
+        System.out.println("MD2");
+    }
+}
